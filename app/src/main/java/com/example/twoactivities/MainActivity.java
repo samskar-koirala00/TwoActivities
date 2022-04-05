4a9a565588e68302704b2d02a5e67dea641bad91
Shadowhunter57 <sanskarkoirala9@gmail.com>
2022-04-05 22:48:45 +0545

    2activitychallenge

--- app/src/main/java/com/example/twoactivities/MainActivity.java
+++ app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -4,53 +4,46 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.os.Parcelable;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
-
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final int TEXT_REQUEST = 1;
-    private TextView mReplyHeadTextView;
-    private TextView mReplyTextView;
-    private EditText mMessageEditText;
-    public static final String EXTRA_MESSAGE =
-            "com.example.android.twoactivities.extra.MESSAGE";
 
-    private static final String LOG_TAG =
-            MainActivity.class.getSimpleName();
+    private final static String LOG_TAG=MainActivity.class.getSimpleName();
+    private final static String EXTRA_MESSAGE ="com.example.android.twoactivitiescodingchallenge.extra.MESSAGE";
+    private  View paragraph;
+    private View article_heading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mMessageEditText = findViewById(R.id.editText_main);
-        mReplyHeadTextView = findViewById(R.id.text_header_reply);
-        mReplyTextView = findViewById(R.id.text_message_reply);
     }
 
-    public void launchSecondActivity(View view) {
-
-        Log.d(LOG_TAG, "Button clicked!");
+    public void LaunchParaTwo(View view) {
+        Log.d(LOG_TAG,"Paragraph Two");
         Intent intent = new Intent(this, SecondActivity.class);
-        String message = mMessageEditText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivityForResult(intent, TEXT_REQUEST);
+        paragraph =findViewById(R.id.article_text);
+        intent.putExtra(EXTRA_MESSAGE, (Parcelable) paragraph);
+        startActivity(intent);
     }
-    @Override
-    public void onActivityResult(int requestCode,
-                                 int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == TEXT_REQUEST) {
-            if (resultCode == RESULT_OK) {
-                String reply =
-                        data.getStringExtra(SecondActivity.EXTRA_REPLY);
-                mReplyHeadTextView.setVisibility(View.VISIBLE);
-                mReplyTextView.setText(reply);
-                mReplyTextView.setVisibility(View.VISIBLE);
-            }
-        }
+
+    public void LaunchParaOne(View view){
+        Log.d(LOG_TAG, "Paragraph One");
+        Intent intent = new Intent(this, SecondActivity.class);
+        paragraph =findViewById(R.id.article_text);
+        intent.putExtra(EXTRA_MESSAGE, (Parcelable) paragraph);
+        startActivity(intent);
+    }
+
+    public void LaunchParaThree(View view){
+        Log.d(LOG_TAG, "Paragraph Three");
+        Intent intent = new Intent(this, SecondActivity.class);
+        paragraph =findViewById(R.id.article_text);
+        intent.putExtra(EXTRA_MESSAGE,(Parcelable) paragraph);
+        startActivity(intent);
     }
 }
